00e2cf63e9c03c2a987216d29a6d274eda751dae
warriorincrystal123 <130286520+warriorincrystal123@users.noreply.github.com>
2023-04-18 14:08:18 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,13 @@ timeanddate.setTime(8, 59, 55, timeanddate.MornNight.AM)
 pins.setAudioPin(AnalogPin.P1)
 music.setVolume(255)
 servos.P0.setAngle(45)
+radio.setGroup(218)
 loops.everyInterval(1000, function () {
     timeanddate.advanceBy(1, timeanddate.TimeUnit.Seconds)
 })
 basic.forever(function () {
     if (timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) == "09:00.00") {
+        radio.sendString("take your meds")
         servos.P0.setAngle(135)
         basic.pause(1000)
         servos.P0.setAngle(45)
@@ -17,7 +19,7 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+    while (pins.digitalReadPin(DigitalPin.P2) == 1 && (timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) >= "09:00.00" && timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) <= "09:05.00")) {
         music.playMelody("C C5 C C5 C C5 C C5 ", 250)
     }
 })
