80a99c750036d4290b1058f6728b4183a41b5241
warriorincrystal123 <130286520+warriorincrystal123@users.noreply.github.com>
2023-04-19 17:20:01 +0800

    Restore '18/04/2023, 21:30:46 Update pxt.json, main.blocks, main.ts, main.py'

--- main.ts
+++ main.ts
@@ -4,20 +4,22 @@ input.onButtonPressed(Button.B, function () {
 timeanddate.setTime(8, 59, 55, timeanddate.MornNight.AM)
 pins.setAudioPin(AnalogPin.P1)
 music.setVolume(255)
-servos.P0.setAngle(45)
+radio.setGroup(218)
 loops.everyInterval(1000, function () {
     timeanddate.advanceBy(1, timeanddate.TimeUnit.Seconds)
 })
 basic.forever(function () {
-    if (timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) == "09:00.00") {
-        servos.P0.setAngle(135)
-        basic.pause(1000)
-        servos.P0.setAngle(45)
-        servos.P0.stop()
+    while (pins.digitalReadPin(DigitalPin.P2) == 1 && (timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) >= "09:00.00" && timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) <= "09:05.00")) {
+        music.playMelody("C C5 C C5 C C5 C C5 ", 250)
     }
 })
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
-        music.playMelody("C C5 C C5 C C5 C C5 ", 250)
+    if (timeanddate.time(timeanddate.TimeFormat.HHMMSS24hr) == "09:00.00") {
+        radio.sendString("take your meds")
+        servos.P0.setAngle(45)
+        basic.pause(900)
+        servos.P0.setAngle(135)
+        basic.pause(800)
+        servos.P0.stop()
     }
 })
